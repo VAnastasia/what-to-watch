95085ba50dba5b7326e0ea033de032a3482ec4b4
Anastasiya <v.anastasiya26@mail.ru>
2020-02-04 23:01:03 +0700

    задание 5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
 
+const promoFilm = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  year: 2014
+};
+
+ReactDOM.render(
+    <App promoFilm={promoFilm} />,
+    document.getElementById(`root`)
+);
