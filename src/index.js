3202302277f0c5ba9d05f155dbdd8f2e235ef873
Anastasiya <v.anastasiya26@mail.ru>
2020-02-07 11:00:53 +0700

    задание 7

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ const promoFilm = {
   year: 2014
 };
 
+const films = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+
+
 ReactDOM.render(
-    <App promoFilm={promoFilm} />,
+    <App promoFilm={promoFilm} films={films} />,
     document.getElementById(`root`)
 );
